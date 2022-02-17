f3e5968afd536f371d5e52bccb866ed48764a92a
liyihehai <liyi_hehai@sina.com>
2022-02-17 10:48:15 +0800

    更改测试

--- src/pages/merchant/setting/MerchantSettingList.tsx
+++ src/pages/merchant/setting/MerchantSettingList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 
 const MerchantSettingList: React.FC = () => {
-  return <PageContainer />;
+  return <PageContainer title={false}/>;
 };
 
 export default MerchantSettingList;
