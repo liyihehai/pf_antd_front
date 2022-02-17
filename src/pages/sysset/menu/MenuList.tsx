48bfc8a7e80db993366730555c3c19f711053889
liyihehai <liyi_hehai@sina.com>
2022-02-17 14:41:03 +0800

    测试提交

--- src/pages/sysset/menu/MenuList.tsx
+++ src/pages/sysset/menu/MenuList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 
 const MenuList: React.FC = () => {
-  return <PageContainer />;
+  return <PageContainer title={false}/>;
 };
 
 export default MenuList;
